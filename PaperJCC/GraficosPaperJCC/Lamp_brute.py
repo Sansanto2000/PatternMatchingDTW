c5b50f3b79ab20bc0d4f8640c098b6db590bc663
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2024-04-01 20:25:35 -0300

    Mejora de los graficos

--- PaperJCC/GraficosPaperJCC/Lamp_brute.py
+++ PaperJCC/GraficosPaperJCC/Lamp_brute.py
@@ -10,7 +10,7 @@ from utils import get_Data_FILE
 act_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Definicion de constantes
-FINDDIR = os.path.join(os.path.dirname(act_dir), 'WCOMPs')
+FINDDIR = r'C:\Users\santi\OneDrive\Documentos\Doctorado\PatternMatchingDTW\WCOMPs'
 FILENAME = "WCOMP01.fits"
 
 # Obtencion de empirico
@@ -18,16 +18,14 @@ obs_x, obs_y, obs_headers = get_Data_FILE(dirpath=FINDDIR, name=FILENAME, normal
 obs_real_x = obs_x * obs_headers['CD1_1'] + obs_headers['CRVAL1']
 
 # grafico de lamapara empirica en bruto (sin calibrar)
-plt.figure(figsize=(12, 4), dpi=800)
+plt.figure(figsize=(12, 4), dpi=1200)
 
-plt.bar([0], [0], width=0, label='Lampara Observada', color='black', align='edge', alpha=1) # Lamp Bruto
-for x, y in zip(obs_x, obs_y):
-    plt.bar([x], [y], width=1, align='edge', color='black', alpha=1)
+plt.bar(obs_x, obs_y, width=1.8, label='Extracted lamp', color='black', align='edge', alpha=1) # Lamp Bruto
 
 plt.xlabel('Nº pixel')
 plt.ylabel('Intensity')
 
 plt.legend()
 
-plt.savefig(os.path.join(act_dir, f"Lamp_brute.png"))
+plt.savefig(os.path.join(act_dir, f"Lamp_brute.svg"))
 plt.close()
