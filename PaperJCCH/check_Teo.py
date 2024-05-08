311451b1985d0e5127f4014be996094c9500992c
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2024-05-07 22:46:13 -0300

    Concatenado de datos teoricos resuelto

--- PaperJCCH/check_Teo.py
+++ PaperJCCH/check_Teo.py
@@ -1,25 +1,31 @@
 import os
+import numpy as np
 import matplotlib.pyplot as plt
-from utils import extract_lamp_info
+from utils import extract_lamp_info, concateAndExtractTeoricalFiles
 
 # Especificar archivo teorico a emplear
 act_dir = os.path.dirname(os.path.abspath(__file__))
 files = [
     os.path.join(act_dir, 'Cartones_Info', 'W_Lamp01.fits'),
     os.path.join(act_dir, 'Cartones_Info', 'W_Lamp02.fits'),
-    #os.path.join(act_dir, 'Cartones_Info', 'W_Lamp03.fits')
+    os.path.join(act_dir, 'Cartones_Info', 'W_Lamp03.fits'),
+    os.path.join(act_dir, 'Cartones_Info', 'W_Lamp04.fits'),
+    os.path.join(act_dir, 'Cartones_Info', 'W_Lamp05.fits'),
+    os.path.join(act_dir, 'Cartones_Info', 'W_Lamp06.fits'),
+    os.path.join(act_dir, 'Cartones_Info', 'W_Lamp07.fits'),
+    os.path.join(act_dir, 'Cartones_Info', 'W_Lamp08.fits'),
+    os.path.join(act_dir, 'Cartones_Info', 'W_Lamp09.fits'),
+    os.path.join(act_dir, 'Cartones_Info', 'W_Lamp10.fits')
     ]
-colors = ['red', 'blue', 'green', 'yellow']
-EPSILON = 0.06
+
+EPSILON = 0.055
 
 plt.figure(figsize=(12, 4), dpi=600) # Ajuste de tamaño de la figura
 
-total_teo_x = []
-total_teo_y = []
+total_x = np.array([])
+total_y = np.array([])
 for i in range(len(files)):
-
-
-
+    
     # Separar informacion
     teo_x, teo_y, teo_head = extract_lamp_info(files[i], normalize=False)
 
@@ -28,14 +34,59 @@ for i in range(len(files)):
         teo_real_x = teo_x * teo_head['CD1_1'] + teo_head['CRVAL1']
     except Exception as e:
         print(f"Error archivo {files[i]} < Falta de headers")
+    
+    if (i==0):
+        total_x = np.array(teo_real_x)
+        total_y = np.array(teo_y)
+    else:
+        inter_total_index = np.where(total_x >= teo_real_x[0])
+        x1 = total_x[inter_total_index]
+        y1 = total_y[inter_total_index]
+        inter_real_index = np.where(teo_real_x <= total_x[-1])
+        x2 = teo_real_x[inter_real_index]
+        y2 = teo_y[inter_real_index]
+        
+        # Combinar arreglos y ordenarlos
+        x_combinado = np.concatenate((x1, x2))
+        y_combinado = np.concatenate((y1, y2))
+        indices_orden = np.argsort(x_combinado)
+        x_combinado = x_combinado[indices_orden]
+        y_combinado = y_combinado[indices_orden]
+        
+        # Filtrar segun EPSILON
+        if(len(x_combinado)==0):
+            x_filtrado = x_combinado
+            y_filtrado = y_combinado
+        else:
+            indices_seleccionados = []
+            for j in range(len(x_combinado)-1):
+                
+                diff = x_combinado[j+1] - x_combinado[j]
+                
+                if diff <= EPSILON:
+                    # Obtener el índice del elemento con el mayor valor en y
+                    indice_max_y = np.argmax(y_combinado[j:j+2]) + j
+                    indices_seleccionados.append(indice_max_y)
+                else:
+                    indices_seleccionados.append(j)
+            indices_seleccionados.append(len(x_combinado)-1)
+            
+            # Crear los arreglos filtrados
+            x_filtrado = x_combinado[indices_seleccionados]
+            y_filtrado = y_combinado[indices_seleccionados]
+        
+        # Concatenamos los arreglos finales
+        indices = np.arange(len(total_x))
+        masc = np.logical_not(np.isin(indices, inter_total_index))
+        x1 = total_x[masc]
+        y1 = total_y[masc]
+        indices = np.arange(len(teo_real_x))
+        masc = np.logical_not(np.isin(indices, inter_real_index))
+        x2 = teo_real_x[masc]
+        y2 = teo_y[masc]
+        total_x = np.concatenate([x1, x_filtrado, x2])
+        total_y = np.concatenate([y1, y_filtrado, y2])
 
-    total_teo_x.extend(teo_real_x)
-    total_teo_y.extend(teo_y)
-
-    print(teo_head['CD1_1'])
-    print(len(teo_x))
-    print(teo_real_x)
-
-    plt.bar(teo_real_x, teo_y, width=1, label='Teorical', color=colors[i], align='edge', alpha=1) 
+plt.bar(total_x, total_y, width=1, label='Teorical', color='blue', align='edge', alpha=1) 
 
 plt.savefig(os.path.join(act_dir, 'check_Teo.svg'))
--- PaperJCCH/utils.py
+++ PaperJCCH/utils.py
@@ -431,3 +431,85 @@ def run_calibrations(teo_x:np.ndarray, teo_y:np.ndarray, files:np.ndarray, windo
     df = df._append(nueva_fila, ignore_index=True)
     df.to_csv(output_csv_path, index=False) # Guardar DataFrame actualizado
     
+def concateAndExtractTeoricalFiles(files:list, epsilon):
+    """Funcion para extraer y concaternar los datos de varios archivos
+    teoricos. Si multiples archivos se superponen entre si entonces 
+    resuelve sus conflictos eligiendo los valores maximos.
+
+    Args:
+        files (_type_): Conjunto de archivos teoricos.
+        epsilon (_type_): Distancia minima que tiene que haber entre
+        2 datos teoricos. Si dos datos estan a menos distancia entonces
+        se deja el que tenga un valor adjunto mayor.
+
+    Returns:
+        numpy.ndarray: Valores para X de la concatenacion realizada.
+        numpy.ndarray: Valores para Y de la concatenacion realizada.
+    """
+
+    total_x = np.array([])
+    total_y = np.array([])
+    for i in range(len(files)):
+        
+        # Separar informacion
+        teo_x, teo_y, teo_head = extract_lamp_info(files[i], normalize=False)
+
+        # Determinar teorico real
+        try:
+            teo_real_x = teo_x * teo_head['CD1_1'] + teo_head['CRVAL1']
+        except Exception as e:
+            print(f"Error archivo {files[i]} < Falta de headers")
+        
+        if (i==0):
+            total_x = np.array(teo_real_x)
+            total_y = np.array(teo_y)
+        else:
+            inter_total_index = np.where(total_x >= teo_real_x[0])
+            x1 = total_x[inter_total_index]
+            y1 = total_y[inter_total_index]
+            inter_real_index = np.where(teo_real_x <= total_x[-1])
+            x2 = teo_real_x[inter_real_index]
+            y2 = teo_y[inter_real_index]
+            
+            # Combinar arreglos y ordenarlos
+            x_combinado = np.concatenate((x1, x2))
+            y_combinado = np.concatenate((y1, y2))
+            indices_orden = np.argsort(x_combinado)
+            x_combinado = x_combinado[indices_orden]
+            y_combinado = y_combinado[indices_orden]
+            
+            # Filtrar segun epsilo
+            if(len(x_combinado)==0):
+                x_filtrado = x_combinado
+                y_filtrado = y_combinado
+            else:
+                indices_seleccionados = []
+                for j in range(len(x_combinado)-1):
+                    
+                    diff = x_combinado[j+1] - x_combinado[j]
+                    
+                    if diff <= epsilon:
+                        # Obtener el índice del elemento con el mayor valor en y
+                        indice_max_y = np.argmax(y_combinado[j:j+2]) + j
+                        indices_seleccionados.append(indice_max_y)
+                    else:
+                        indices_seleccionados.append(j)
+                indices_seleccionados.append(len(x_combinado)-1)
+                
+                # Crear los arreglos filtrados
+                x_filtrado = x_combinado[indices_seleccionados]
+                y_filtrado = y_combinado[indices_seleccionados]
+            
+            # Concatenamos los arreglos finales
+            indices = np.arange(len(total_x))
+            masc = np.logical_not(np.isin(indices, inter_total_index))
+            x1 = total_x[masc]
+            y1 = total_y[masc]
+            indices = np.arange(len(teo_real_x))
+            masc = np.logical_not(np.isin(indices, inter_real_index))
+            x2 = teo_real_x[masc]
+            y2 = teo_y[masc]
+            total_x = np.concatenate([x1, x_filtrado, x2])
+            total_y = np.concatenate([y1, y_filtrado, y2])
+
+    return total_x, total_y
